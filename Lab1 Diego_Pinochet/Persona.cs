3cd3cabeaa4cdf6c6806c4ca6a4cf84ae2ccb5d8
Diego Pinochet <dppinochet@miuandes.cl>
2020-03-17 17:04:36 -0300

    Primer commit

---
+++ Lab1 Diego_Pinochet/Persona.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_Diego_Pinochet
+{
+    class Persona
+    {
+        public string Name;
+        public string LastName;
+
+        public void Lanzar()
+        {
+            Random random = new Random();
+            Console.WriteLine(random.Next(0, 3));
+        }
+    }
+}
---
+++ Lab1 Diego_Pinochet/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_Diego_Pinochet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona person = new Persona();
+            person.Name = "Bob";
+            person.Name = "Kunga";
+
+            person.Lanzar();
+
+        }
+    }
+}
